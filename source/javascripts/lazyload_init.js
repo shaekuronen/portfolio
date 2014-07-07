e3ad53da9553e184d4ec97f27160091b87bb1d9e
Shae Kuronen <shae.kuronen@gmail.com>
2014-07-06 18:17:41 -0700

    updated lazyload init

--- source/javascripts/lazyload_init.js
+++ source/javascripts/lazyload_init.js
@@ -1,11 +1,5 @@
 ;(function($) {
 
-  $('.lazyload').lazyload({
-    threshold : 200,
-    failure_limit : 10,
-    skip_invisible : true,
-    effect : 'show',
-    event : 'scroll'
-  });
+  $('.lazyload').lazyload();
 
 }(jQuery));
